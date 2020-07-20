5b7210870dbead324ec745945291ef3aa9d62a5a
suspir0n <evandrojunior1615@gmail.com>
2020-07-19 21:00:42 -0300

    finalize the API, add a package body-parser for tessting the api in insomnia

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser({ extended: true }));
+
+//GET
 app.get('/', (req, res) => {
     res.status(200).send('Olá Mundo');
 })
 
+//POST
+app.post('/', (req, res) => {
+    res.status(201).send(req.body);
+});
+
+//PUT
+app.put('/:id', (req, res) => {
+    res.status(202).send({
+        codigo: req.params.id,
+        corpo: req.body
+    });
+});
+
+//DELETE
+app.delete('/:id', (req, res) =>{
+    res.status(204).send(req.params.id);
+});
+
+//INIT
 app.listen(3000, () => {
     console.log('api starting');
 });
+
+/*
+STATUS CODE:
+
+200 - OK
+201 - Created
+202 - Accepted
+204 - No Content
+401 -  Error
+
+POST = 201
+GET = 200
+PUT = 202
+DELETE  = 204
+*/
